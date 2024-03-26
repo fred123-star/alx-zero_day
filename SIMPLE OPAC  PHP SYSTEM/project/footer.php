b41993d9083e41410c15aeec36fbd54459910af1
fred123-star <126969246+fred123-star@users.noreply.github.com>
2024-03-26 17:04:18 +0300

    Add files via upload

---
+++ SIMPLE OPAC  PHP SYSTEM/project/footer.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title></title>
+   <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+   
+</body>
+</html>
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shelve</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">borrow</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +254716977800 </p>
+         <p> <i class="fas fa-phone"></i> +254798150713 </p>
+         <p> <i class="fas fa-envelope"></i> fredrickopailo@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Nairobi </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/profile.php?id=100062287403241"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="twitter.com"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="instagram.com/ema.khan.35912"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>The programmer...</span> </p>
+
+</section>
